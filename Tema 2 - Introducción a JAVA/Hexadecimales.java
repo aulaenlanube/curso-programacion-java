d498d5f8fdcb930121c627657ac0df7b6dc7369c
aulaenlanube <torregrosaedu@gmail.com>
2022-10-06 10:49:57 +0200

    video 2-7 curso java

---
+++ Tema 2 - Introducción a JAVA/Hexadecimales.java
@@ -0,0 +1,17 @@
+public class Hexadecimales {
+
+    public static void main(String[] args) {
+        
+        byte num1 = 0x02;           // almacena un 2
+        byte num2 = 0x0B;           // almacena un 11
+        byte num3 = 0x10;           // almacena un 16
+        byte num4 = 0x12;           // almacena un 18
+        byte num5 = (byte)0xFF;     // almacena un -1
+        byte num6 = 0x7F;           // almacena un 127
+        byte num7 = (byte)0x80;     // almacena un -128
+        byte num8 = 0x012;          // almacena un 18
+        //byte num9 = 0x112;        // ERROR
+       
+    }
+    
+}
--- Tema 2 - Introducción a JAVA/HolaMundo.java
+++ Tema 2 - Introducción a JAVA/HolaMundo.java
@@ -1,7 +1,6 @@
 //primer programa en JAVA
 class HolaMundo{
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         System.out.println("Hola Mundo!");
     }
 }
