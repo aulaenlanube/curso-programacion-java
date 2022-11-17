2efcb6db0f0fea72272765930bd86932506c7392
aulaenlanube <edtorlla@gmail.com>
2022-11-17 16:48:12 +0100

    video 4-3 curso java v2

--- Tema 4 - Arrays y matrices/Ejercicios/EjerciciosIniciacion.java
+++ Tema 4 - Arrays y matrices/Ejercicios/EjerciciosIniciacion.java
@@ -1,29 +1,54 @@
-import java.lang.reflect.Array;
-
 public class EjerciciosIniciacion {
 
     public static void main(String[] args) {
 
-        int[] array =  { 1, 2, 3};
-        int[] array2 = { 8, 1};
+        int[] array = { 1, 2, 3 };
+        int[] array2 = { 8, 1 };
         ejercicio1(array, 5);
         String[] palabras = { "á", "ñabb", "Áaa", "Ñbb" };
         ejercicio2(palabras);
         ejercicio3(palabras, 'ñ');
-        
-        
         mostrarArray(ejercicio4(array, array2));
-        mostrarArray(ejercicio4_2(array, array2));      
-
-
+        mostrarArray(ejercicio4_2(array, array2));
     }
 
     static void mostrarArray(int[] n) {
-        for(int i=0;i<n.length;i++)
-        {
-            System.out.print(n[i]+" ");
+        for (int i = 0; i < n.length; i++) {
+            System.out.print(n[i] + " ");
         }
         System.out.println();
+    }
+
+    static void ejercicio1(int[] array, int n) {
+
+        int contador = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == n)
+                contador++;
+        }
+        if (contador == 0)
+            System.out.println("El valor " + n + " no aparece en el Array.");
+        else
+            System.out.println("El valor " + n + " aparece " + contador + " " + (contador == 1 ? "vez" : "veces")
+                    + " en el Array.");
+    }
+
+    static void ejercicio2(String[] palabras) {
+
+        String palabraMax = "";
+        for (int i = 0; i < palabras.length; i++) {
+            if (palabras[i].length() > palabraMax.length())
+                palabraMax = palabras[i];
+        }
+        System.out.println("La palabra más larga es \"" + palabraMax + "\".");
+    }
+
+    static void ejercicio3(String[] palabras, char letra) {
+
+        for (int i = 0; i < palabras.length; i++) {
+            if (palabras[i].toLowerCase().charAt(0) == Character.toString(letra).toLowerCase().charAt(0))
+                System.out.println(palabras[i]);
+        }
     }
 
     static int[] ejercicio4(int[] a, int[] b) {
@@ -64,37 +89,4 @@ public class EjerciciosIniciacion {
         }
         return c;
     }
-
-    static void ejercicio3(String[] palabras, char letra) {
-
-        for (int i = 0; i < palabras.length; i++) {
-            if (palabras[i].toLowerCase().charAt(0) == Character.toString(letra).toLowerCase().charAt(0))
-                System.out.println(palabras[i]);
-        }
-    }
-
-    static void ejercicio2(String[] palabras) {
-
-        String palabraMax = "";
-        for (int i = 0; i < palabras.length; i++) {
-            if (palabras[i].length() > palabraMax.length())
-                palabraMax = palabras[i];
-        }
-        System.out.println("La palabra más larga es \"" + palabraMax + "\".");
-    }
-
-    static void ejercicio1(int[] array, int n) {
-
-        int contador = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == n)
-                contador++;
-        }
-        if (contador == 0)
-            System.out.println("El valor " + n + " no aparece en el Array.");
-        else
-            System.out.println("El valor " + n + " aparece " + contador + " " + (contador == 1 ? "vez" : "veces")
-                    + " en el Array.");
-    }
-
 }
