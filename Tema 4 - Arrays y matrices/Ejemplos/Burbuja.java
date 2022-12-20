694bb4395f5e010de118164b86d1a3f9cf6b650c
aulaenlanube <edtorlla@gmail.com>
2022-12-20 12:37:04 +0100

    video 4-22 curso java

---
+++ Tema 4 - Arrays y matrices/Ejemplos/Burbuja.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Burbuja {
+
+    public static void main(String[] args) {
+
+        // Declaración e inicialización
+        int[] arrayEjemplo = { 6, 5, 4, 3, 2, 1 };
+        int[] arrayOrdenado;
+
+        // Salida inicial
+        System.out.println("Sin ordenar: ");
+        System.out.println(Arrays.toString(arrayEjemplo));
+
+        // Ordenación
+        arrayOrdenado = burbuja(arrayEjemplo);
+
+        // Salida final
+        System.out.println("Ordenado: ");
+
+        System.out.println(Arrays.toString(arrayOrdenado));
+    }
+
+    public static int[] burbuja(int[] vector) {
+
+        int aux;
+        for (int i = 0; i < vector.length; i++) {
+            for (int j = 0; j < vector.length - i - 1; j++) {
+                // El valor máximo lo más a la derecha posible
+                if (vector[j] > vector[j + 1]) {
+                    aux = vector[j];
+                    vector[j] = vector[j + 1];
+                    vector[j + 1] = aux;
+                    System.out.println(Arrays.toString(vector));
+                }
+            }
+        }
+        return vector;
+    }
+}
---
+++ Tema 4 - Arrays y matrices/Ejercicios/GeneredorArrays.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class GeneredorArrays {
+
+    public static void main(String[] args) {
+
+        int[][] nums = generarMatriz(10, 5, 9);
+        for (int[] fila : nums) {
+            System.out.println(Arrays.toString(fila));
+        }
+    }
+
+    static int[][] generarMatriz(int lado, int numMax) {
+        return generarMatriz(lado, lado, lado, lado, numMax);
+    }
+
+    static int[][] generarMatriz(int ancho, int alto, int numMax) {
+        return generarMatriz(ancho, ancho, alto, alto, numMax);
+    }
+
+    /**
+     * Genera una matriz irregular con enteros aleatorios
+     * 
+     * @param anchoMin - Anchura mínima de las filas de la matriz, si se pone un
+     *                 cero, pueden haber filas sin elementos, los valores negativos
+     *                 se convierten a 0 de forma automática. Debe ser igual o
+     *                 menor a anchoMax, de lo contrario, se establecerá anchoMin al
+     *                 mismo valor que anchoMax
+     * @param anchoMax - Anchura máxima de las filas de la matriz, debe ser igual o
+     *                 mayor a anchoMin, de lo contrario, se establecerá anchoMin al
+     *                 mismo valor que anchoMax. Los valores negativos se convierten
+     *                 a 0 de forma automática.
+     * @param altoMin  - Altura mínima de la matriz, debe ser 1 o superior, de lo
+     *                 contrario puede no tener filas. Los valores negativos se
+     *                 convierten a 0 de forma automática. Debe ser igual o
+     *                 menor a altoMax, de lo contrario, se establecerá anchoMin al
+     *                 mismo valor que anchoMax
+     * @param altoMax  - Cantidad máxima de filas de la matriz, debe ser igual o
+     *                 mayor a altoMin, de lo contrario, se establecerá altoMin al
+     *                 valor de altoMax, los valores negativos se convierten a 0 de
+     *                 forma automática
+     * @param numMax   - Valor máximo para cada uno de los enteros de la matriz, los
+     *                 valores posibles serán desde 0 hasta numMax
+     * @return Matriz con enteros generados de forma aleatoria
+     */
+    static int[][] generarMatriz(int anchoMin, int anchoMax, int altoMin, int altoMax, int numMax) {
+
+        // si hay negativos los convertimos a 0
+        if (anchoMin < 0)
+            anchoMin = 0;
+        if (anchoMax < 0)
+            anchoMax = 0;
+        if (altoMin < 0)
+            altoMin = 0;
+        if (altoMax < 0)
+            altoMax = 0;
+
+        // si los mínimos son mayores, los igualamos
+        if (anchoMin > anchoMax)
+            anchoMin = anchoMax;
+        if (altoMin > altoMax)
+            altoMin = altoMax;
+
+        // generamos matriz
+        Random r = new Random();
+        int[][] matriz = new int[r.nextInt(altoMin, altoMax + 1)][];
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i] = new int[r.nextInt(anchoMin, anchoMax + 1)];
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = r.nextInt(numMax + 1);
+            }
+        }
+        return matriz;
+    }
+
+}
