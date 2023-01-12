c0343e60d88835322ba9324efaadbe154e14d9a7
aulaenlanube <edtorlla@gmail.com>
2023-01-12 10:46:02 +0100

    video 4-26 curso java

---
+++ Tema 4 - Arrays y matrices/Generadores y Bordes/GeneradorArrays.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class GeneradorArrays {
+
+    /**
+     * Crea un Array con enteros aleatorios
+     * 
+     * @param elementos - Cantidad de elementos del Array
+     * @return - Array con los enteros generados
+     */
+    static int[] generarArray(int elementos) {
+        Random r = new Random();
+        int[] array = new int[elementos];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt(elementos);
+        }
+        return array;
+    }
+
+    /**
+     * Crea un Array con enteros aleatorios
+     * 
+     * @param elementos - Cantidad de elementos del Array
+     * @param numMax    - Valor máximo de cada uno de los elementos del Array, el
+     *                  valor estará entre 0 y numMax
+     * @return - Array con los enteros generados
+     */
+    static int[] generarArray(int elementos, int numMax) {
+        Random r = new Random();
+        int[] array = new int[elementos];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt(numMax + 1);
+        }
+        return array;
+    }
+
+}
