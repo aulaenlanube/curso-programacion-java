de3e02eaf81b3d99515c628ce40823bed61a57ba
aulaenlanube <edtorlla@gmail.com>
2023-01-20 13:32:41 +0100

    video 5-8 curso java

---
+++ Tema 5 - Introducción a la POO/Ejemplos/Circulo.java
@@ -0,0 +1,41 @@
+public class Circulo {
+    private double radio;
+    private String color;
+    private int centroX, centroY;
+
+    public Circulo() { // crea un círculo de radio 50, negro y centro en (100,100)
+        radio = 50;
+        color = "negro";
+        centroX = 100;
+        centroY = 100;
+    }
+
+    public Circulo(double r) { // crea un círculo de radio 50, negro y centro en (100,100)
+        setRadio(r);
+        color = "blanco";
+        centroX = 0;
+        centroY = 0;
+    }
+
+    public double getRadio() { // consulta el radio del círculo
+        return radio;
+    }
+
+    public void setRadio(double nuevoRadio) { // actualiza el radio del círculo a nuevoRadio
+        if (nuevoRadio >= 0)
+            radio = nuevoRadio;
+    }
+
+    public void decrece() { // decrementa el radio del círculo
+        radio = radio / 1.3;
+    }
+
+    public double area() { // calcula el área del círculo
+        return Math.PI * radio * radio;
+    }
+
+    public void mostrarDatos() { // obtiene un String con las componentes del círculo
+        System.out.println("Círculo de radio " + radio + ", color " + color + " y centro (" + centroX + "," + centroY
+                + "), y su área es " + area());
+    }
+}
---
+++ Tema 5 - Introducción a la POO/Ejemplos/EjemploCirculo.java
@@ -0,0 +1,14 @@
+public class EjemploCirculo {
+
+    public static void main(String[] args) {
+
+        // Crear un círculo
+        Circulo c1 = new Circulo();       
+        Circulo c2 = new Circulo(125);  
+        c1.mostrarDatos();        
+        c2.mostrarDatos();
+        
+
+    }
+
+}
