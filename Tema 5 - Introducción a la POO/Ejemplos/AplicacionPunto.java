d64ed13198aa6fe5d71cea88ffe5601d1a106293
aulaenlanube <edtorlla@gmail.com>
2023-01-30 11:27:13 +0100

    video 5-15 curso java

--- Tema 5 - Introducción a la POO/Ejemplos/AplicacionPunto.java
+++ Tema 5 - Introducción a la POO/Ejemplos/AplicacionPunto.java
@@ -1,7 +1,47 @@
 public class AplicacionPunto {
 
     public static void main(String[] args) {
-        
+
+        int num = 1;
+        String nombre = "Los Strings son inmutables";
+        Punto p1 = new Punto(2, "p1");
+        Punto p2 = new Punto(1, 3, "p2");
+        Punto p3 = new Punto(1, "p3");
+
+        // p1.mostrarDatos();
+        // p2.mostrarDatos();
+        // p3.mostrarDatos();
+
+        modificarPunto(p1);
+        modificarEntero(num);
+        modificarString(nombre);
+
+        System.out.println("Punto.x=" + p1.getX());
+        System.out.println("num=" + num);
+        System.out.println("nombre="+nombre);
+
+        System.out.println(p1);
     }
-    
+
+    public static void mostrarPuntos(Punto... puntos) {
+
+        for (Punto p : puntos) {
+            p.mostrarDatos();
+
+        }
+    }
+
+    public static void modificarPunto(Punto p) {
+        p.setX(999);
+
+    }
+
+    public static void modificarEntero(int n) {
+        n = 999;
+    }
+
+    public static void modificarString(String s) {
+        s = "String modificada";
+    }
+
 }
--- Tema 5 - Introducción a la POO/Ejemplos/Punto.java
+++ Tema 5 - Introducción a la POO/Ejemplos/Punto.java
@@ -1,22 +1,107 @@
 public class Punto {
 
+    private static int puntos = 1;
+
     public int x;
     public int y;
+    public String nombre;
 
-    public Punto(int a) {
-        x = a;
-        y = a;
+
+    public Punto(int x, int y, String nombre) {
+        this(x,y);
+        this.nombre = nombre;
+        puntos++;
+    }
+    
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+        puntos+=2;
     }
 
-    public Punto(int a, int b) {
-        x = a;
-        y = b;
+    public Punto(int x) {
+        this.x = x;
+        this.y = x;
+        puntos+=3;
     }
+
+    public Punto(int x, String nombre) {
+        this(x);
+        this.nombre = nombre;
+        puntos+=4;
+    }
+
+    
 
     public double calcularDistanciaCentro() {
         double z;
         z = Math.sqrt((x * x) + (y * y));
         return z;
+    }
+
+    public void mostrarDatos() {
+        if (this.nombre == null)
+            System.out.println("El punto no tiene nombre, sus datos son:");
+        else
+            System.out.println("El punto de nombre " + nombre + " tiene los siguientes datos:");
+        System.out.println("X=" + x);
+        System.out.println("Y=" + y);
+        System.out.println("La distancia respecto al centro es " + calcularDistanciaCentro());
+        getPuntos();
+        System.out.println("--------------------------------------------------------------");
+    }
+
+    public static void getPuntos() {        
+        System.out.println("Puntos="+puntos);
+    }
+
+    /**
+     * @param puntos the puntos to set
+     */
+    public static void setPuntos(int puntos) {
+        Punto.puntos = puntos;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
 }
