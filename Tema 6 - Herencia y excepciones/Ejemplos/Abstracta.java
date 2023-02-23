126be2cd0cb4ab5c42078322513bacf0c80e15cd
aulaenlanube <edtorlla@gmail.com>
2023-02-23 13:01:24 +0100

    video 6-11 curso java

---
+++ Tema 6 - Herencia y excepciones/Ejemplos/Abstracta.java
@@ -0,0 +1,7 @@
+public abstract class Abstracta {
+
+    void met(){
+        System.out.println("met");
+    }
+    
+}
---
+++ Tema 6 - Herencia y excepciones/Ejemplos/Excepciones.java
@@ -0,0 +1,26 @@
+import java.io.FileReader;
+
+public class Excepciones {
+
+    public static void main(String[] args) {
+
+        // excepciones no verificadas
+        try {
+            int num1 = 2;
+            int[] arr = new int[3];
+            int num2 = arr[5]; // Se producirá una ArrayIndexOutOfBoundsException
+            System.out.println(num1 + num2);
+
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error en las excepciones no verificadas");            
+        }
+
+        // excepciones verificadas
+        try {
+            FileReader file = new FileReader("archivo.txt");
+            file.close();
+        } catch (Exception e) {
+            System.out.println("He capturado la excepción");
+        }
+    }
+}
