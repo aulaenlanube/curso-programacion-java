ce50057fc90fdf779920acee1a3e19de72ed083c
aulaenlanube <edtorlla@gmail.com>
2023-02-28 22:00:16 +0100

    video 6-14 curso java v2

--- Tema 3 - Métodos en JAVA/Ejercicios/SumarNumeros.java
+++ Tema 3 - Métodos en JAVA/Ejercicios/SumarNumeros.java
@@ -1,3 +1,12 @@
+/**
+ * 
+ * Crea un método que obtenga la suma de los números naturales desde 1 hasta N.
+ * Se debe pasar como parámetro el número N, debe ser mayor que cero. Se debe
+ * imprimir toda la cadena por consola. Por ejemplo, para N=4 → ( 1+2+3+4 = 10).
+ * 
+ * @author Edu Torregrosa Llácer - aulaenlanube.com
+ */
+
 public class SumarNumeros {
 
     public static void main(String[] args) {
