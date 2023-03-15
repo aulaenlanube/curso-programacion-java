298a23a7ba0d3f485cbde83e6915b8f503e73039
aulaenlanube <edtorlla@gmail.com>
2023-03-15 20:26:21 +0100

    video 7-4 curso java

--- Tema 7 - Colecciones/Ejemplos/Alumno.java
+++ Tema 7 - Colecciones/Ejemplos/Alumno.java
@@ -1,4 +1,4 @@
-public class Alumno {
+public class Alumno implements Comparable<Alumno> {
     private String nombre;
     private String nia;
     private int edad;
@@ -9,51 +9,40 @@ public class Alumno {
         this.edad = edad;
     }
 
-    /**
-     * @return the nombre
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the nia
-     */
     public String getNia() {
         return nia;
     }
 
-    /**
-     * @param nia the nia to set
-     */
     public void setNia(String nia) {
         this.nia = nia;
     }
 
-    /**
-     * @return the edad
-     */
     public int getEdad() {
         return edad;
     }
 
-    /**
-     * @param edad the edad to set
-     */
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
     @Override
     public String toString() {
-        return "Alumno [nombre=" + nombre + ", nia=" + nia + ", edad=" + edad + "]";
+        return "[nombre=" + nombre + ", edad="+edad+"]";
+    }
+
+    @Override
+    public int compareTo(Alumno a) {
+        int comparacion = Integer.compare(a.getEdad(), this.edad); 
+        if (comparacion == 0) comparacion = nombre.compareTo(a.getNombre());     
+        return comparacion;       
     }
     
 }
--- Tema 7 - Colecciones/Ejemplos/alumnos/iterator.java
+++
@@ -1,5 +0,0 @@
-package alumnos;
-
-public class iterator {
-
-}
