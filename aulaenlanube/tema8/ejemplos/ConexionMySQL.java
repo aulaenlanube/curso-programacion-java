1518e5ba7c79b54b714761f223a87bd4d1812c6f
aulaenlanube <edtorlla@gmail.com>
2023-05-29 10:24:15 +0200

    ejemplos mysql curso java v1

---
+++ aulaenlanube/tema8/ejemplos/ConexionMySQL.java
@@ -0,0 +1,51 @@
+package aulaenlanube.tema8.ejemplos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConexionMySQL {
+
+    //Datos conexión con la BD
+    public static final String USER = "root";
+    public static final String PWD = "";
+    public static final String URL = "jdbc:MySQL://localhost/agenda";
+
+    //Datos del contacto a insertar
+    public static final String NOMBRE = "Pepe";
+    public static final String CORREO = "Pepe@pepe.com";
+    public static final int TELF = 123456789;
+
+    public static void main(String[] args) {
+
+        try {
+
+            //realizamos connexión
+            Connection conex = DriverManager.getConnection(URL, USER, PWD);        
+
+            // insertamos contacto
+            String queryInsert = "INSERT INTO contacto(nombre, correo) VALUES ('"+NOMBRE+"', '"+CORREO+"')";
+            conex.createStatement().executeUpdate(queryInsert);
+
+            // consultamos contactos
+            String querySelect = "SELECT nombre, correo FROM contacto";
+            ResultSet resultado = conex.createStatement().executeQuery(querySelect);
+
+            // recorremos los datos obtenidos
+            while (resultado.next()) {
+                String nombre = resultado.getString("nombre");
+                String correo = resultado.getString("correo");
+                System.out.println("NOMBRE: " + nombre);
+                System.out.println("CORREO: " + correo);
+            }
+
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+        }       
+    }
+}
---
+++ aulaenlanube/tema8/ejemplos/ConexionMySQLImagen.java
@@ -0,0 +1,62 @@
+package aulaenlanube.tema8.ejemplos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import aulaenlanube.tema4.bordes.Bordes;
+
+public class ConexionMySQLImagen {
+
+    // Datos conexión con la BD
+    public static final String USER = "root";
+    public static final String PWD = "";
+    public static final String URL = "jdbc:MySQL://localhost/agenda";
+
+    // Datos del contacto a insertar
+    public static final String NOMBRE = "Pepe";
+    public static final String CORREO = "Pepe@pepe.com";
+    public static final int TELF = 123456789;
+    public static final String RUTA_IMAGEN = "aulaenlanube/tema8/imagenes/default.jpg";
+
+    public static void main(String[] args) {
+
+        try {
+
+            Connection conex = DriverManager.getConnection(URL, USER, PWD);
+
+            // imagen
+            final byte[] DATOS_IMAGEN = Files.readAllBytes(Paths.get(RUTA_IMAGEN));
+
+            // insertamos contacto
+            String queryInsert = "INSERT INTO contacto(nombre, correo, telf, imagen) VALUES (?,?,?,?)";
+            PreparedStatement queryFinal = conex.prepareStatement(queryInsert);
+            queryFinal.setString(1, NOMBRE);
+            queryFinal.setString(2, CORREO);
+            queryFinal.setInt(3, TELF);
+            queryFinal.setBytes(4, DATOS_IMAGEN);
+            queryFinal.executeUpdate();
+
+            // consultamos contactos
+            String querySelect = "SELECT nombre, correo, telf FROM contacto";
+            ResultSet resultado = conex.createStatement().executeQuery(querySelect);
+
+            // recorremos los datos obtenidos
+            while (resultado.next()) {
+                String datosContactoActual = "";
+                datosContactoActual += "NOMBRE: " + resultado.getString("nombre") + "\n";
+                datosContactoActual += "CORREO: " + resultado.getString("correo") + "\n";
+                datosContactoActual += "TELF: " + resultado.getInt("telf") + "\n";
+                Bordes.mostrarTextoConBordes(datosContactoActual);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
