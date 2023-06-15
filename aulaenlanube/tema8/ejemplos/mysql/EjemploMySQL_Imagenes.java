6888b6cf16805e8ba9aee066ed70c3dfb7896f85
aulaenlanube <edtorlla@gmail.com>
2023-06-15 11:08:28 +0200

    video 8-18 curso java v2

--- aulaenlanube/tema8/ejemplos/mysql/EjemploMySQL_Imagenes.java
+++ aulaenlanube/tema8/ejemplos/mysql/EjemploMySQL_Imagenes.java
@@ -1,8 +1,12 @@
 package aulaenlanube.tema8.ejemplos.mysql;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -80,6 +84,78 @@ public class EjemploMySQL_Imagenes {
 
             // mostramos datos del contacto
             contacto.mostrarDatos();
+        }
+    }
+
+    ///////////////////////////
+    // VÍDEO 8.18  
+    ///////////////////////////
+
+    public static void descargarImagenesContactosBD(Connection conex) throws SQLException, IOException {
+
+        // obtenemos información de la imágenes de los contactos
+        String querySelect = "SELECT imagen, binarioImagen FROM contacto";
+        PreparedStatement queryFinalSelect = conex.prepareStatement(querySelect);
+        ResultSet contactos = queryFinalSelect.executeQuery();
+
+        // recorremos los contactos obtenidos
+        while (contactos.next()) {
+
+            Blob blob = contactos.getBlob("binarioImagen");
+            byte[] blobBytes = blob.getBytes(1, (int) blob.length());
+
+            // Especifica la ruta y el nombre del archivo donde quieres guardar el BLOB
+            String rutaImagen = contactos.getString("imagen");
+
+            // Usa FileOutputStream para escribir los bytes en un archivo
+            FileOutputStream fos = new FileOutputStream(rutaImagen);
+            fos.write(blobBytes);
+            fos.close();
+        }
+
+    }
+
+    public static void cargarImagenesContactosBD(Connection conex, String rutaCarpeta) {
+
+        File carpeta = new File(rutaCarpeta);
+
+        // si es una carpeta
+        if (carpeta.isDirectory()) {
+
+            // obtenemos todos los archivos
+            File[] archivos = carpeta.listFiles();
+
+            // por cada archivo
+            for (File archivo : archivos) {
+
+                // si es una imagen jpg, creamos el contacto
+                if (archivo.isFile() && esJpg(archivo.getName())) {
+                    Contacto contacto = generarContactoAleatorio();
+                    contacto.setImagen(archivo.getPath());
+                    insertarContactoBD(conex, contacto);
+                }
+            }
+        } else
+            System.out.println("La ruta proporcinada no es una carpeta válida");
+    }
+
+    public static boolean esJpg(String filename) {
+        if (filename.length() < 4) {
+            return false;
+        } else {
+            String extension = filename.substring(filename.length() - 4);
+            return extension.equalsIgnoreCase(".jpg");
+        }
+    }
+
+    public static void mostrarImagenContacto(Connection conex, Contacto contacto) {
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().open(new File(contacto.getImagen()));
+            } else
+                throw new Exception();
+        } catch (Exception e) {
+            System.out.println("ERROR: no se ha podido mostrar la imagen del contacto");
         }
     }
 }
