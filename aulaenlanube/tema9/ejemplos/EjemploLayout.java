daf22b0955ecaa7f164c0c2ec7f7494c63a4964d
aulaenlanube <edtorlla@gmail.com>
2023-06-29 11:42:42 +0200

    video 9-2 curso java

--- aulaenlanube/tema9/ejemplos/EjemploLayout.java
+++ aulaenlanube/tema9/ejemplos/EjemploLayout.java
@@ -1,5 +1,6 @@
 package aulaenlanube.tema9.ejemplos;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class EjemploLayout {
@@ -8,9 +9,17 @@ public class EjemploLayout {
 
         // creamos ventana
         JFrame ventana = new JFrame("Mi primera ventana");
-        ventana.setVisible(true);
-        ventana.setSize(500, 200);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // características ventana
+        ventana.setSize(500, 500);
+        ventana.setVisible(true);
+        ventana.setLayout(null);
+
+        // creamos boton, posición x:100, y:200 - botón de 200px de ancho y 40px de alto
+        JButton boton = new JButton("Pulsa");
+        ventana.add(boton);
+        boton.setBounds(100, 200, 200, 40);
     }
 
 }
