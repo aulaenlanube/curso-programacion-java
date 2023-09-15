a8ce1d587089675708f441ff0d59ee73c33240c8
aulaenlanube <edtorlla@gmail.com>
2023-09-15 14:18:25 +0200

    video 9-26 v4

--- aulaenlanube/tema9/javafx/css/src/main/java/com/aula/css/App.java
+++ aulaenlanube/tema9/javafx/css/src/main/java/com/aula/css/App.java
@@ -3,9 +3,7 @@ package com.aula.css;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
